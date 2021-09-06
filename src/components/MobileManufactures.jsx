95dbae080e952d3b2fc41c6c2465045d7ee07fca
Mrrupendra <rupendrasingh112233@gmail.com>
2021-09-07 02:20:43 +0530

    react lists done

---
+++ src/components/MobileManufactures.jsx
@@ -0,0 +1,17 @@
+function MobileManufactures(){
+
+  
+    return (
+        <div>
+            <h1>Mobile Manufactures</h1>
+            <ul>
+                <li style={{listStyle: "square"}}>Samsung</li>
+                <li style={{listStyle: "square"}}>HTC</li>
+                <li>Micromax</li>
+                <li style={{listStyle: "circle"}}>Apple</li>
+            </ul>
+        </div>
+    );
+}
+
+export default MobileManufactures;
